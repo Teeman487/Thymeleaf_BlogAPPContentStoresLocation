2cbcab9ea31ee1bc608d27e850d55249b5c9df3c
Akinade Toheeb Adebusoye <adebusoyeteeman@gmail.com>
2023-08-20 10:11:50 +0100

    T

---
+++ src/main/java/net/ummuaqibatulkhair/thymeleaf_blogappcontentstoreslocation/ThymeleafBlogAppContentStoresLocationApplication.java
@@ -0,0 +1,13 @@
+package net.ummuaqibatulkhair.thymeleaf_blogappcontentstoreslocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafBlogAppContentStoresLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafBlogAppContentStoresLocationApplication.class, args);
+	}
+
+}
